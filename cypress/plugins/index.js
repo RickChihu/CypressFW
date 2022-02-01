099c27b00a27d0333cf39801e3ed084b55a168d7
Rick Chihu <RickChihu@gmail.com>
2022-02-01 11:59:27 -0600

    Report generated

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -25,4 +25,22 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
+
+  require('@cypress/code-coverage/task')(on, config)
+
+  // add other tasks to be registered here
+
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
+  return config
 }
+
+// module.exports = (on, config) => {
+//   require('@cypress/code-coverage/task')(on, config)
+
+//   // add other tasks to be registered here
+
+//   // IMPORTANT to return the config object
+//   // with the any changed environment variables
+//   return config
+// }
